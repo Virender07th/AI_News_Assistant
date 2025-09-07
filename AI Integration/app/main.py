3c2e1e0118322296280a6ce7d9c3c043c8dd13a4
VirenderThakur <hs374639@gmail.com>
2025-09-08 00:35:51 +0530

    Adding cors in fastapi backend

--- AI Integration/app/main.py
+++ AI Integration/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-news-assistant-2-7q8g.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
